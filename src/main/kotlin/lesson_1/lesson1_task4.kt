fbd64b79bf4a17b63ec5152ba9534a8edb9dd2db
telegacosta <temkur@ya.ru>
2023-05-10 19:06:58 +0300

    KS-1-2 data from second lesson

---
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,18 @@
+package lesson_1
+
+fun main() {
+
+    val length: Long = 40_868_600_000 //расстояние, которое пролетел Гагарин пока был в космосе в миллиметрах;
+    val age: Byte = 27 //возраст Гагарина на момент полета (целых лет);
+    val partOfDay: Float = 0.075F //какую часть дня Гагарин провел в космосе;
+    val seconds: Short = 6480 //количество секунд которое Гагарин провел в космосе;
+    val partOfYear: Double = 2.0547945205479453E-4 //какую часть года Гагарин провел в космосе;
+    val apogee: Int = 327000 //апогей орбиты Гагарина в метрах.
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+}
