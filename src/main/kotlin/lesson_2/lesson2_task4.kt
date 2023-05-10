2e11e6a556f482af151b6edb971e84fcdfb25f0c
telegacosta <temkur@ya.ru>
2023-05-10 22:24:34 +0300

    KS-2-4 data from second lesson

---
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -0,0 +1,11 @@
+package lesson_2
+
+fun main() {
+
+    val crystalOre = 7
+    val ironOre = 11
+    val buff = 20
+
+    println("Кристаллическая руда: ${crystalOre*buff/100}")
+    println("Железная руда: ${ironOre*buff/100}")
+}
