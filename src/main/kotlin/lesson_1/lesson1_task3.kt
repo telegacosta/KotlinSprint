ba5214b10538ca00cfeed514e68db471feedf0a9
telegacosta <temkur@ya.ru>
2023-05-10 08:14:38 +0300

    KS-1-2 data from second lesson

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,18 @@
+package lesson_1
+
+fun main() {
+
+    val year = 1961
+    var hour = "09"
+    var minute = "07"
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = "10"
+    minute = "55"
+
+    println("$hour:$minute")
+
+}
