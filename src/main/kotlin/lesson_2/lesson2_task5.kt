8425b5f2a2a4b0cf98ed9de2e750a503e37beb43
telegacosta <temkur@ya.ru>
2023-05-10 23:15:24 +0300

    KS-2-4 data from second lesson

---
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -0,0 +1,13 @@
+package lesson_2
+
+import kotlin.math.pow
+
+fun main() {
+
+    val sum = 70000
+    val rate = 16.7
+    val term = 20
+    val result = sum * ((1 + rate/100)).pow(term)
+
+    println(println("%.3f".format(result)))
+}
