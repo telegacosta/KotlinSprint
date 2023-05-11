145a863c13fab12132c0f530268786016ba87555
telegacosta <temkur@ya.ru>
2023-05-12 00:43:48 +0300

    KS-3-1 data from third lesson

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,16 @@
+package lesson_3
+
+fun main() {
+
+    var name = "Андреева Татьяна Сергеевна"
+    var age = 20
+    var info = "ФИО в $age: $name"
+
+    println(info)
+
+    name = "Сидорова Татьяна Сергеевна"
+    age = 22
+    info = "ФИО в $age: $name"
+
+    println(info)
+}
