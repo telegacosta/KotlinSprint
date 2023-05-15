2389ad1a67bd0f1979f0eb2a9431bfa5e5d5e71f
telegacosta <temkur@ya.ru>
2023-05-15 09:15:07 +0300

    KS-4-5 data from forth lesson

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -2,10 +2,6 @@ package lesson_4
 
 fun main() {
 
-    val minNumberOfCrew = 55 // минимальная численность экипажа
-    val maxNumberOfCrew = 70 // максимальная численность экипажа
-    val minNumberOfBoxes = 50 // минимальное количество ящиков провизии
-
     println("Есть повреждения корпуса (true/false):")
     val isDamage= readln().toBoolean()
     println("Введите численность экипажа:")
@@ -15,10 +11,14 @@ fun main() {
     println("Благоприятная погода (true/false):")
     val isGoodWeather = readln().toBoolean()
 
-    val condition = (!isDamage && (numberOfCrew in minNumberOfCrew until maxNumberOfCrew)
-            && (numberOfBoxes > minNumberOfBoxes))
-            || (isGoodWeather && (numberOfCrew == maxNumberOfCrew)
-            && (numberOfBoxes > minNumberOfBoxes))
+    val condition = (!isDamage && (numberOfCrew in MIN_NUMBER_OF_CREW until MAX_NUMBER_OF_CREW)
+            && (numberOfBoxes > MIN_NUMBER_OF_BOXES))
+            || (isGoodWeather && (numberOfCrew == MAX_NUMBER_OF_CREW)
+            && (numberOfBoxes > MIN_NUMBER_OF_BOXES))
 
     println("Можно отправляться: $condition")
 }
+
+const val MIN_NUMBER_OF_CREW = 55 // минимальная численность экипажа
+const val MAX_NUMBER_OF_CREW = 70 // максимальная численность экипажа
+const val MIN_NUMBER_OF_BOXES = 50 // минимальное количество ящиков провизии
