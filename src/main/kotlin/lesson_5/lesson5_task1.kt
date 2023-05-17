a28d9925c2ae2007a98e776db503d97aa13c1e4f
telegacosta <temkur@ya.ru>
2023-05-17 12:49:02 +0300

    KS-5-1 data from fifth lesson

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -3,14 +3,12 @@ package lesson_5
 fun main() {
 
     val number1 = (1..100).random()
-        println("Число №1: $number1")
-
     val number2 = (1..100).random()
-        println("Число №2: $number2")
 
-    println("Введите сумму чисел №1 и №2:")
+    println("Решите математический пример:")
+    print("$number1 + $number2 = ")
+
     val sum = readln().toInt()
-
     val isSumRight = sum == number1 + number2
 
     if (isSumRight)
